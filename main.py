d3ca894dc41bb4536a232694d7481231a7687051
tracerwhite24 <tracerwhite@gmail.com>
2025-02-19 21:05:29 +0300

    Update main.py
    Функция запуска

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+import random
 
+
+def heads_or_tails():
+    flip = random.choice(["heads", "tails"])
+    return flip
+
+result = heads_or_tails()
+
+def importer():
+    if result == "heads":
+        import rando
+    else:
+        import toep
+    import recognizer
+
+importer()
 
