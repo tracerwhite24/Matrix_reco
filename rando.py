49499883368c78e251685d27169d96895ae9f115
tracerwhite24 <tracerwhite@gmail.com>
2025-02-19 21:06:55 +0300

    Update rando.py

--- rando.py
+++ rando.py
@@ -7,9 +7,9 @@ low = 1
 high = 9
 matrix = np.random.randint(low, high, size=(rows, cols))
 
-print(f"Матрица, заполненная случайными целыми числами от {low} до {high-1}:")
-print(matrix)
-print("-" * 30)
+# print(f"Матрица, заполненная случайными целыми числами от {low} до {high-1}:")
+# print(matrix)
+# print("-" * 30)
 
 np.save('matrix.npy', matrix)
 print("Матрица сохранена в matrix.npy")
